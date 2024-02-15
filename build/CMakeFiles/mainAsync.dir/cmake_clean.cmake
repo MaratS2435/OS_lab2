caf5aa46656f4db660aa8360c43d71fd8132d16d
Marat Sisenov <cool.marats@yandex.ru>
2024-02-15 18:02:53 +0300

    false doc

---
+++ build/CMakeFiles/mainAsync.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainAsync.dir/mainAsync.cpp.o"
+  "CMakeFiles/mainAsync.dir/mainAsync.cpp.o.d"
+  "mainAsync"
+  "mainAsync.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainAsync.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mainSync.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainSync.dir/mainSync.cpp.o"
+  "CMakeFiles/mainSync.dir/mainSync.cpp.o.d"
+  "mainSync"
+  "mainSync.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainSync.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
